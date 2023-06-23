a428520836b68a0c0e1c3053062e9a0f52b4d47a
MarianCbm <cioara.mariann@yahoo.com>
2023-06-24 01:23:25 +0300

    Add a function in JS called game

--- RPS.js
+++ RPS.js
@@ -3,6 +3,11 @@ function getComputerChoice () {
     return choices[Math.floor(Math.random()*4)]
 }
 
+function takeInput () {
+    userInput=prompt("Rock, Paper or Scissors");
+    userInput=userInput.toLocaleLowerCase();
+    return userInput;
+}
 
 function checkInput(){
     if (userInput=="rock" || userInput=="paper" || userInput=="scissors") {
@@ -14,11 +19,7 @@ function checkInput(){
 
 }
 
-function takeInput () {
-    userInput=prompt("Rock, Paper or Scissors");
-    userInput=userInput.toLocaleLowerCase();
-    return userInput;
-}
+
 
 
 function playRound(playerSelection, computerSelection){
@@ -58,7 +59,9 @@ function playRound(playerSelection, computerSelection){
         }
 
         else {
-            console.log("Wrong input")
+            console.log("Wrong input, Try again")
+            playRound(y=takeInput(),x=getComputerChoice())
+            
         }
 
 
@@ -67,9 +70,42 @@ function playRound(playerSelection, computerSelection){
 
 
 
+function game () {
+    let round = 0;
+    let lostRounds=0;
+    let drawRounds=0;
+    while (round<5) {
+        result= playRound(y=takeInput(),x=getComputerChoice());
+        if (result =="You Lose! Paper beats Rock"|| result == "You Lose! Scissors beats Paper" || result == "You Lose! Rock beats Scissors"  ) {
+            lostRounds = lostRounds+1;
+        }
+        else if (result =="Draw!" ) {
+            drawRounds= drawRounds++;
+        }
+        round=round+1 ;
+        console.log(`Round ${ round } elapsed `)
+    }
+    if (lostRounds>=3) {
+        console.log ("You lost this game!")
+    }
+    else if (lostRounds==2 || drawRounds==2){
+        console.log ("You lost this game!")
+
+    }
+    else if (lostRounds==1 || drawRounds==4) {
+        console.log ("You lost this game!")
+
+    }
+    else {
+        console.log("You won this game!")
+    }
+
+
+}
+
+game(); 
 
 
 
-result= playRound(y=takeInput(),x=getComputerChoice());
-console.log(result)
+
 
