7569f6dbb623d3312ad95dd4c944566872fb43e7
MarianCbm <cioara.mariann@yahoo.com>
2023-08-02 01:15:25 +0300

    Add paragraph with game results

--- RPS.js
+++ RPS.js
@@ -5,6 +5,7 @@ function getComputerChoice () {
 
 let userInput;
 
+
 const rbtn=document.querySelector("#rock")
 const pbtn=document.querySelector("#paper")
 const sbtn=document.querySelector("#scissors")
@@ -28,47 +29,73 @@ sbtn.addEventListener('click',()=>{
 })
 
 
+const container = document.querySelector('#result');
+const result = document.createElement("p");
+result.style.fontSize="30px";
+container.appendChild(result);
+
+
+
+
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection==="rock"){
         if (computerSelection==="paper"){
             console.log("You Lost! Paper beats rock!");
+            result.textContent="You Lost! Paper beats rock!";
+            result.style.color="red";
             return ("You Lost! Paper beats rock!");
         }
         else if (computerSelection=="scissors"){
             console.log("You Won! Rock beats scissors!");
+            result.textContent="You Won! Rock beats scissors!";
+            result.style.color="blue";
             return ("You Won! Rock beats scissors!");
         }
         else {
             console.log("Draw");
+            result.textContent="DRAW!";
+            result.style.color="green";
             return ("Draw");
         }
     }
     else if (playerSelection==="paper"){
         if (computerSelection==="scissors"){
             console.log("You Lost! Scissors beats paper!");
+            result.textContent="You Lost! Scissors beats paper!";
+            result.style.color="red";
             return ("You Lost! Scissors beats paper!");
         }
         else if (computerSelection=="rock"){
             console.log("You Won! Paper beats rock!");
+            result.textContent="You Won! Paper beats rock!";
+            result.style.color="blue";
             return ("You Won! Rock beats scissors!");
         }
         else {
             console.log("Draw");
+            result.textContent="DRAW!";
+            result.style.color="green";
             return ("Draw"); 
         }
     }
     else if (playerSelection==="scissors"){
         if (computerSelection==="rock"){
             console.log("You Lost! Rock beats scissors!");
+            result.textContent="You Lost! Rock beats scissors!";
+            result.style.color="red";
             return ("You Lost! Rock beats scissors!");
         }
         else if (computerSelection=="paper"){
             console.log("You Won! Scissors beats paper!");
+            result.textContent="You Won! Scissors beats paper!";
+            result.style.color="blue";
             return ("You Won! Scissors beats paper!");
         }
         else {
             console.log("Draw");
+            result.textContent="DRAW!";
+            result.style.color="green";
             return ("Draw"); 
         }
     }
@@ -84,7 +111,7 @@ function playRound(playerSelection, computerSelection){
             if (result =="You Lose! Paper beats Rock"|| result == "You Lose! Scissors beats Paper" || result == "You Lose! Rock beats Scissors"  ) {
                 lostRounds = lostRounds+1;
             }
-            else if (result =="Draw!" ) {
+            else if (result =="Draw!" ) {margin:auto;
                 drawRounds= drawRounds++;
             }
             round=round+1 ;
